5b8bdd7eedb52229e4a35b0c3eee8b1d16cc7ac9
Den Lipovsky <den@denlipovsky.com>
2012-03-30 09:46:43 -0400

    adding tmux config file

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,4 @@ ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 mkdir -p ~/.vim
 ln -sf ~/dotfiles/vim/pathogen/autoload ~/.vim/autoload
 ln -sf ~/dotfiles/vim/bundle ~/.vim/bundle
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
