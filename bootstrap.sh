4bcaf1188a7da00ebf08a1fbebf194fd7d3f7883
Den Lipovsky <den@denlipovsky.com>
2012-05-22 10:12:04 -0400

    some additional tmux commands show message after reloading config split window using h and v copy and paste work like vim (v, y, p)

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,6 @@ mkdir -p ~/.vim
 ln -sf ~/dotfiles/vim/pathogen/autoload ~/.vim/autoload
 ln -sf ~/dotfiles/vim/bundle ~/.vim/bundle
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+
+git submodule init
+git submodule update
